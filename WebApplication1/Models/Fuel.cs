48f15184f492a6a7f32867f7002406ac6bc51893
CMTharindu888 <tharindu.mudalige@sl.crystal-martin.com>
2024-09-19 10:11:12 +0530

    new

--- WebApplication1/Models/Fuel.cs
+++ WebApplication1/Models/Fuel.cs
@@ -6,11 +6,11 @@ namespace WebApplication1.Models
     {
         [Key] public int id { get; set; }
 
-        public string fuelstationname { get; set; }
-        public string district { get; set; }
+        public string fuelstationname { get; set; } 
+        public string district { get; set; }  
         public string addresse { get; set; }
-
-        public string fuelamt { get; set; }
+ 
+        public string fuelamt { get; set; } 
 
         public string email { get; set; }
 
